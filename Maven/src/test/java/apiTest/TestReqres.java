eb9fa40bf8ce54d5ee3d5a3b63e7137a498d1461
IBoaghe <ion.boaghe@orange.com>
2024-05-18 11:44:08 +0300

    add testng.xml

---
+++ Maven/src/test/java/apiTest/TestReqres.java
@@ -0,0 +1,13 @@
+package apiTest;
+
+import org.testng.annotations.Test;
+
+public class TestReqres {
+
+
+    @Test
+    public void testTestNG(){
+        System.out.println("Test success");
+
+    }
+}
